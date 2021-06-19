f44303c462d586d6d9857fb2a20bdb8d0ddb6d99
Grégoire Chevalier <aagchevalier@gmail.com>
2021-06-19 12:11:39 +0200

    2021-06-19 # 1 : Création de la structure nuxt et vue

---
+++ siteGreg/components/TheNavbar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-purple-600 w-5/5 h-12">
+        test
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ siteGreg/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-screen h-screen bg-gray-800">
+    <the-navbar />
+  </div>
+</template>
+
+<script>
+import TheNavbar from '../components/TheNavbar.vue'
+export default {
+  components: { TheNavbar },}
+</script>
